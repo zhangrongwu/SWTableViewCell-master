ae0d428c50705c03d95bc1fcbaa90e78203d8e7d
zhangrongwu <zhangrongwuios@sina.com>
2014-11-30 17:56:02 +0800

    自定义cell
    自定义cell可以进行多个按钮点击

---
+++ SWTableViewCell/ViewController.m
@@ -0,0 +1,175 @@
+//
+//  ViewController.m
+//  SWTableViewCell
+//
+//  Created by Chris Wendel on 9/10/13.
+//  Copyright (c) 2013 Chris Wendel. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "SWTableViewCell.h"
+
+@interface ViewController () {
+    NSMutableArray *_testArray;
+}
+
+@property (nonatomic, weak) IBOutlet UITableView *tableView;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    self.tableView.rowHeight = 90;
+    self.tableView.allowsSelection = NO; // We essentially implement our own selection
+    /**
+     *设置cell上分割线的位置等数据.....
+     */
+    self.tableView.separatorInset = UIEdgeInsetsMake(0, 0, 0, 0); // Makes the horizontal row seperator stretch the entire length of the table view
+    
+    _testArray = [[NSMutableArray alloc] init];
+    
+    // Add test data to our test array
+    [_testArray addObject:[NSDate date]];
+    [_testArray addObject:[NSDate date]];
+    [_testArray addObject:[NSDate date]];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark UITableViewDataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return _testArray.count;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    NSLog(@"cell selected at index path %d", indexPath.row);
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *cellIdentifier = @"Cell";
+    
+    SWTableViewCell *cell = (SWTableViewCell *)[tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    if (cell == nil) {
+        NSMutableArray *leftUtilityButtons = [NSMutableArray new];
+        NSMutableArray *rightUtilityButtons = [NSMutableArray new];
+        
+        [leftUtilityButtons addUtilityButtonWithColor:
+         [UIColor colorWithRed:0.07 green:0.75f blue:0.16f alpha:1.0]
+                                                 icon:[UIImage imageNamed:@"check.png"]];
+        [leftUtilityButtons addUtilityButtonWithColor:
+         [UIColor colorWithRed:1.0f green:1.0f blue:0.35f alpha:1.0]
+                                                 icon:[UIImage imageNamed:@"clock.png"]];
+        [leftUtilityButtons addUtilityButtonWithColor:
+         [UIColor colorWithRed:1.0f green:0.231f blue:0.188f alpha:1.0]
+                                                 icon:[UIImage imageNamed:@"cross.png"]];
+        [leftUtilityButtons addUtilityButtonWithColor:
+         [UIColor colorWithRed:0.55f green:0.27f blue:0.07f alpha:1.0]
+                                                 icon:[UIImage imageNamed:@"list.png"]];
+        
+        [rightUtilityButtons addUtilityButtonWithColor:
+         [UIColor colorWithRed:0.78f green:0.78f blue:0.8f alpha:1.0]
+                                                 title:@"More"];
+        [rightUtilityButtons addUtilityButtonWithColor:
+         [UIColor colorWithRed:1.0f green:0.231f blue:0.188 alpha:1.0f]
+                                                 title:@"Delete"];
+        
+        
+        
+        /**
+         *关闭左右的cell点击按钮 .
+         */
+        cell = [[SWTableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle
+                                      reuseIdentifier:cellIdentifier
+                                  containingTableView:_tableView // Used for row height and selection
+                                   leftUtilityButtons:leftUtilityButtons
+                                  rightUtilityButtons:rightUtilityButtons];
+        cell.delegate = self;
+    }
+    
+    NSDate *dateObject = _testArray[indexPath.row];
+    cell.textLabel.text = [dateObject description];
+    cell.detailTextLabel.text = @"Some detail text";
+    
+    return cell;
+}
+
+-(void)scrollViewWillBeginDragging:(UIScrollView *)scrollView {
+    NSLog(@"scroll view did begin dragging");
+}
+
+/**
+ *设置cell的颜色....
+ */
+- (void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Set background color of cell here if you don't want white
+    
+}
+
+#pragma mark - SWTableViewDelegate
+/**
+ *自定义cell的协议方法
+ */
+- (void)swippableTableViewCell:(SWTableViewCell *)cell didTriggerLeftUtilityButtonWithIndex:(NSInteger)index {
+    switch (index) {
+        case 0:
+            NSLog(@"第0个button");
+            break;
+        case 1:
+            NSLog(@"第1个button");
+            break;
+        case 2:
+            NSLog(@"第2个button");
+            break;
+        case 3:
+            NSLog(@"第3个button");
+        default:
+            break;
+    }
+}
+/**
+ *自定义cell的协议方法
+ */
+- (void)swippableTableViewCell:(SWTableViewCell *)cell didTriggerRightUtilityButtonWithIndex:(NSInteger)index {
+    switch (index) {
+        case 0:
+        {
+            NSLog(@"More button was pressed");
+            UIAlertView *alertTest = [[UIAlertView alloc] initWithTitle:@"Hello" message:@"More more more" delegate:nil cancelButtonTitle:@"cancel" otherButtonTitles: nil];
+            [alertTest show];
+            [cell hideUtilityButtonsAnimated:YES];
+            break;
+        }
+        case 1:
+        {
+            // Delete button was pressed
+            NSIndexPath *cellIndexPath = [self.tableView indexPathForCell:cell];
+            
+            [_testArray removeObjectAtIndex:cellIndexPath.row];
+            
+            /**
+             *删除cell的方式动画
+             */
+            [self.tableView deleteRowsAtIndexPaths:@[cellIndexPath] withRowAnimation:UITableViewRowAnimationFade];
+            break;
+        }
+        default:
+            break;
+    }
+}
+
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    return YES;
+}
+
+@end
